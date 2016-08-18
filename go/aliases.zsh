0bc0ecd8e4b2f420e0660bb642ee522aa0bad126
Peiman Khorramshahi <peiman@khorramshahi.com>
2016-08-18 23:05:37 +0200

    Add go list dependencies aliases.

--- go/aliases.zsh
+++ go/aliases.zsh
@@ -2,3 +2,8 @@
 function gotagsthis() {
   gotags -tag-relative=true -R=true -sort=true -f="tags" -fields=+l .;
 }
+
+
+alias go-list-deps="go list -f '{{join .Imports \"\n\"}}' | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}'"
+
+alias go-list-deps-all="go list -f '{{join .Deps \"\n\"}}' | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}'"
